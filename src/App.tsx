3738fd82a5081e19b3b670507e2d8cbc07664f78
Mitch Kosowski <mitchkosowski@gmail.com>
2019-07-18 15:14:35 -0400

    formatting for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React, { FunctionComponent } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
-import About from './components/about'; 
-import Validators from './components/Validators'; 
+import About from './components/about';
+import Validators from './components/Validators';
 
 const App: React.FC = () => {
   return (
-    <Router> 
+    <Router>
       <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/eth/validators">Active Validators</Link>
+            </li>
+          </ul>
+        </nav>
         <header className="App-header">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/eth/validators">Active Validators</Link>
-              </li>
-            </ul>
-          </nav>
+          <Route path="/about" component={About} />
+          <Route path="/eth/validators" component={Validators} />
         </header>
-        <Route path="/about" component={About} />
-        <Route path="/eth/validators" component={Validators} />
       </div>
     </Router>
   );
