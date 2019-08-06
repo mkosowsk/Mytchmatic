e8b07de98a6540df83500d0a1bee0410d7761758
Mitch Kosowski <mitchkosowski@gmail.com>
2019-08-06 17:48:45 -0400

    working on ValidatorAssignments table

--- src/components/ValidatorAssignments.tsx
+++ src/components/ValidatorAssignments.tsx
@@ -28,6 +28,67 @@ const DEFAULT_QUERY = '';
 //       "publicKey": "sao25T46/2oUBPjzBKa+U0K2lEptQjPxxmPhX5h6ZJj97fNVaIWyFqfHXafGBmoY"
 //     },
 
+const headerRow = [
+  'Public Key',
+  'Crosslink Committees',
+  'Shard',
+  'Slot',
+  'Proposer'
+]
+
+const renderBodyRow = ({
+  publicKey,
+  crosslinkCommittees,
+  shard,
+  slot,
+  proposer
+}: {
+  publicKey: string,
+  crosslinkCommittees: Array<string>,
+  shard: string,
+  slot: string,
+  proposer: string
+},
+  i: number) => ({
+    key: publicKey || `row-${i}`,
+    cells: [
+      publicKey,
+      crosslinkCommittees,
+      shard,
+      slot,
+      proposer
+    ],
+  })
+
+const ValidatorAssignments = () =>
+  <div>
+    <Table
+      celled headerRow={headerRow}
+      renderBodyRow={renderBodyRow}
+      tableData={tableData}
+    />
+    <div className="ui one column padded centered grid">
+      <Table.Footer>
+        <Table.Row>
+          <Table.HeaderCell>
+            <Menu>
+              <Menu.Item as='a' icon>
+                <Icon name='chevron left' />
+              </Menu.Item>
+              <Menu.Item as='a'>1</Menu.Item>
+              <Menu.Item as='a'>2</Menu.Item>
+              <Menu.Item as='a'>3</Menu.Item>
+              <Menu.Item as='a'>4</Menu.Item>
+              <Menu.Item as='a' icon>
+                <Icon name='chevron right' />
+              </Menu.Item>
+            </Menu>
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Footer>
+    </div>
+  </div>
+
 interface IState {
   data: {
     "epoch": string,
@@ -99,76 +160,3 @@ export default ValidatorAssignments;
       </ul> */}
 
 // const tableData = validators.validators;
-
-// const headerRow = [
-//   'Public Key1',
-//   'Withdrawal Credentials',
-//   'Activation Eligibility Epoch',
-//   'Activation Epoch',
-//   'Exit Epoch',
-//   'Withdrawal Epoch',
-//   'Slashed',
-//   'Effective Balance'
-// ]
-
-// const renderBodyRow = ({
-//   public_key,
-//   withdrawal_credentials,
-//   activation_eligiblity_epoch,
-//   activation_epoch,
-//   exit_epoch,
-//   withdrawable_epoch,
-//   slashed,
-//   effective_balance
-// }: {
-//   public_key: string,
-//   withdrawal_credentials: string,
-//   activation_eligiblity_epoch: string,
-//   activation_epoch: string,
-//   exit_epoch: string,
-//   withdrawable_epoch: string,
-//   slashed: boolean,
-//   effective_balance: string
-// },
-//   i: number) => ({
-//     key: public_key || `row-${i}`,
-//     cells: [
-//       public_key,
-//       withdrawal_credentials,
-//       activation_eligiblity_epoch,
-//       activation_epoch,
-//       exit_epoch,
-//       withdrawable_epoch,
-//       slashed,
-//       effective_balance
-//     ],
-//   })
-
-// const ValidatorAssignments = () =>
-//   <div>
-//     <Table
-//       celled headerRow={headerRow}
-//       renderBodyRow={renderBodyRow}
-//       tableData={tableData}
-//     />
-//     <div className="ui one column padded centered grid">
-//       <Table.Footer>
-//         <Table.Row>
-//           <Table.HeaderCell>
-//             <Menu>
-//               <Menu.Item as='a' icon>
-//                 <Icon name='chevron left' />
-//               </Menu.Item>
-//               <Menu.Item as='a'>1</Menu.Item>
-//               <Menu.Item as='a'>2</Menu.Item>
-//               <Menu.Item as='a'>3</Menu.Item>
-//               <Menu.Item as='a'>4</Menu.Item>
-//               <Menu.Item as='a' icon>
-//                 <Icon name='chevron right' />
-//               </Menu.Item>
-//             </Menu>
-//           </Table.HeaderCell>
-//         </Table.Row>
-//       </Table.Footer>
-//     </div>
-//   </div>
