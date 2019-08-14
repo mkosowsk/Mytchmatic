20c5e766fb965e80ed5d2232cca7e1eaef42cc08
Mitch Kosowski <mitchkosowski@gmail.com>
2019-08-14 17:59:13 -0400

    started work on Beacon Blocks

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Attestations from './components/Attestations';
 import Validators from './components/Validators';
 import ValidatorAssignments from './components/ValidatorAssignments';
 import { Menu } from 'semantic-ui-react';
+import Blocks from './components/Blocks'
 
 //TODO: fix hitting both eth/validators and eth/validators/assignments
 //which causes 2 tables to render
@@ -25,12 +26,16 @@ const App: React.FC = () => {
           <Menu.Item as={Link} to="/beacon/attestations">
             Beacon Attestations
           </Menu.Item>
+          <Menu.Item as={Link} to="/beacon/blocks">
+            Beacon Blocks
+          </Menu.Item>
           <Menu.Item as={Link} to="/eth/validators/assignments">
             Validator Assignments
           </Menu.Item>
         </Menu>
         <Route path="/about" component={About} />
         <Route exact path="/beacon/attestations" component={Attestations} />
+        <Route exact path="/beacon/blocks" component={Blocks} />
         <Route exact path="/eth/validators" component={Validators} />
         <Route path="/eth/validators/assignments" component={ValidatorAssignments} />
       </div>
