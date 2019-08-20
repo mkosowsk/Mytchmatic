1bb55f2d677b79ca6bed2563f10477cd62642df8
Mitch Kosowski <mitchkosowski@gmail.com>
2019-08-20 18:05:14 -0400

    working on breaking Attestation Pool into a real component

--- src/components/AttestationsPool.tsx
+++ src/components/AttestationsPool.tsx
@@ -1,8 +1,10 @@
-import React from 'react'; // importing FunctionComponent
+import React, { Component } from 'react'; // importing FunctionComponent
 import Attestations from '../mock/beacon_attestations.json';
 import { Icon, Menu, Table } from 'semantic-ui-react';
 
 // make a complex structured table
+
+// map tableData onto an array
 
 const tableData = Attestations.attestations;
 
@@ -39,62 +41,79 @@ const renderBodyRow = ({
     ],
   })
 
+interface IState {
+  data: {
+    attestations: [
+      {
+        "aggregation_bits": string
+      }
+    ]
+  }
+}
+
+interface IProps { }
 
 // 13 total columns
+class AttestationsPool extends Component<IProps, IState> {
 
-const AttestationsPool = () =>
-  <div>
-    <Table celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell rowSpan='2' textAlign='center'>Aggregation Bits</Table.HeaderCell>
-          <Table.HeaderCell rowSpan='2' textAlign='center'>Beacon Block Root</Table.HeaderCell>
-          <Table.HeaderCell colSpan='2' textAlign='center'>Source</Table.HeaderCell>
-          <Table.HeaderCell colSpan='2' textAlign='center'>Target</Table.HeaderCell>
-          <Table.HeaderCell colSpan='5' textAlign='center'>CrossLink</Table.HeaderCell>
-          <Table.HeaderCell rowSpan='2' textAlign='center'>Custody Bits</Table.HeaderCell>
-          <Table.HeaderCell rowSpan='2' textAlign='center'>Signature</Table.HeaderCell>
-        </Table.Row>
-        <Table.Row>
-          <Table.HeaderCell textAlign='center'>Epoch</Table.HeaderCell>
-          <Table.HeaderCell textAlign='center'>Root</Table.HeaderCell>
-          <Table.HeaderCell textAlign='center'>Epoch</Table.HeaderCell>
-          <Table.HeaderCell textAlign='center'>Root</Table.HeaderCell>
-          <Table.HeaderCell textAlign='center'>Shard</Table.HeaderCell>
-          <Table.HeaderCell textAlign='center'>Parent Root</Table.HeaderCell>
-          <Table.HeaderCell textAlign='center'>Start Epoch</Table.HeaderCell>
-          <Table.HeaderCell textAlign='center'>End Epoch</Table.HeaderCell>
-          <Table.HeaderCell textAlign='center'>Data Root</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-    </Table>
-    <Table
-      celled structured
-      renderBodyRow={renderBodyRow}
-      tableData={tableData}
-    >
-    </Table>
-    <div className="ui one column padded centered grid">
-      <Table.Footer>
-        <Table.Row>
-          <Table.HeaderCell>
-            <Menu>
-              <Menu.Item as='a' icon>
-                <Icon name='chevron left' />
-              </Menu.Item>
-              <Menu.Item as='a'>1</Menu.Item>
-              <Menu.Item as='a'>2</Menu.Item>
-              <Menu.Item as='a'>3</Menu.Item>
-              <Menu.Item as='a'>4</Menu.Item>
-              <Menu.Item as='a' icon>
-                <Icon name='chevron right' />
-              </Menu.Item>
-            </Menu>
-          </Table.HeaderCell>
-        </Table.Row>
-      </Table.Footer>
-    </div>
-  </div>
+  render() {
 
+    return (
+      <div>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell rowSpan='2' textAlign='center'>Aggregation Bits</Table.HeaderCell>
+              <Table.HeaderCell rowSpan='2' textAlign='center'>Beacon Block Root</Table.HeaderCell>
+              <Table.HeaderCell colSpan='2' textAlign='center'>Source</Table.HeaderCell>
+              <Table.HeaderCell colSpan='2' textAlign='center'>Target</Table.HeaderCell>
+              <Table.HeaderCell colSpan='5' textAlign='center'>CrossLink</Table.HeaderCell>
+              <Table.HeaderCell rowSpan='2' textAlign='center'>Custody Bits</Table.HeaderCell>
+              <Table.HeaderCell rowSpan='2' textAlign='center'>Signature</Table.HeaderCell>
+            </Table.Row>
+            <Table.Row>
+              <Table.HeaderCell textAlign='center'>Epoch</Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>Root</Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>Epoch</Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>Root</Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>Shard</Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>Parent Root</Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>Start Epoch</Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>End Epoch</Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>Data Root</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+        </Table>
+        <Table
+          celled structured
+          renderBodyRow={renderBodyRow}
+          tableData={tableData}
+        >
+        </Table>
+        <div className="ui one column padded centered grid">
+          <Table.Footer>
+            <Table.Row>
+              <Table.HeaderCell>
+                <Menu>
+                  <Menu.Item as='a' icon>
+                    <Icon name='chevron left' />
+                  </Menu.Item>
+                  <Menu.Item as='a'>1</Menu.Item>
+                  <Menu.Item as='a'>2</Menu.Item>
+                  <Menu.Item as='a'>3</Menu.Item>
+                  <Menu.Item as='a'>4</Menu.Item>
+                  <Menu.Item as='a' icon>
+                    <Icon name='chevron right' />
+                  </Menu.Item>
+                </Menu>
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Footer>
+        </div>
+      </div>
+    )
+  }
+
+}
 
 export default AttestationsPool;
